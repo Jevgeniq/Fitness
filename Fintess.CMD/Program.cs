f43d739d4ef8addb136b17e3f3bcefbb4066dcc8
Jevgeni Barkin <Jevgeni.Barkin@tptlive.ee>
2019-04-18 18:36:01 +0300

    Реализовано чтение существующего пользователя и добавление нового

---
+++ Fintess.CMD/Program.cs
@@ -0,0 +1,74 @@
+﻿using Fitness.BL.Controller;
+using Fitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintess.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует приложение Fitness");
+
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+
+            var userController = new UserController(name);
+            if (userController.IsNewUser)
+            {
+                Console.Write("Введите пол: ");
+                var gender = Console.ReadLine();
+                var birthDate = ParseDateTime();
+                var weight = ParseDouble("вес");
+                var height = ParseDouble("Рост");
+
+
+                userController.SetNewUserData(gender, birthDate, weight, height);
+            }
+
+            Console.WriteLine(userController.CurrentUser);
+            Console.ReadLine();            
+
+        }
+
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Введите вашу дату рождения (dd.MM.yyyy): ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения");
+                }
+            }
+
+            return birthDate;
+        }
+
+        private static double ParseDouble(string name) 
+        {
+            while (true)
+            {
+                Console.Write($"Введите {name}");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}");
+                }
+            }
+
+        }
+    }
+}
